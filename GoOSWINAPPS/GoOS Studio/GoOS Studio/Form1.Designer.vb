fd302088c2e0f2e0cff62e815a5d62e29396b82d
Owen Benyon <fulptdm@gmail.com>
2021-10-08 16:48:16 +0100

    Test Commit for discord webhook

--- GoOSWINAPPS/GoOS Studio/GoOS Studio/Form1.Designer.vb
+++ GoOSWINAPPS/GoOS Studio/GoOS Studio/Form1.Designer.vb
@@ -22,10 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(814, 461)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
